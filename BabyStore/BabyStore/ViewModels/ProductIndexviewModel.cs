e0ccd391139e1e48f9c49bb09c2b377f82056968
Mohamed <mohamed.sellahi@gmail.com>
2017-11-14 21:40:40 +0100

    End of paging

--- BabyStore/BabyStore/ViewModels/ProductIndexviewModel.cs
+++ BabyStore/BabyStore/ViewModels/ProductIndexviewModel.cs
@@ -4,30 +4,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace BabyStore.ViewModels {
-   public class ProductIndexviewModel {
-      public IQueryable<Product> Products { get; set; }
-      public string Search { get; set; }
-      public IEnumerable<CategoryWithCount> CatsWithCount { get; set; }
-      public string Category { get; set; }
+  public class ProductIndexviewModel {
 
-      public IEnumerable<SelectListItem> CatFilterItems {
-         get {
-            var allCats = CatsWithCount.Select(cc => new SelectListItem {
-               Value = cc.CategoryName,
-               Text = cc.CatNamesWithCount
-            });
-            return allCats;
-         }
-      }
-   }
+    public IPagedList<Product> Products { get; set; }
+    public string Search { get; set; }
+    public IEnumerable<CategoryWithCount> CatsWithCount { get; set; }
+    public string Category { get; set; }
+    public string SortBy { get; set; }
+    public Dictionary<string,string> Sorts { get; set; }
 
-   public class CategoryWithCount {
-      public int ProductCount { get; set; }
-      public string CategoryName { get; set; }
-      public string CatNamesWithCount {
-         get { return CategoryName + "(" + ProductCount.ToString() + ")"; }
+
+    public IEnumerable<SelectListItem> CatFilterItems {
+      get {
+        var allCats = CatsWithCount.Select(cc => new SelectListItem {
+          Value = cc.CategoryName,
+          Text = cc.CatNamesWithCount
+        });
+        return allCats;
       }
-   }
+    }
+  }
+
+  public class CategoryWithCount {
+    public int ProductCount { get; set; }
+    public string CategoryName { get; set; }
+    public string CatNamesWithCount {
+      get { return CategoryName + "(" + ProductCount.ToString() + ")"; }
+    }
+  }
 }
