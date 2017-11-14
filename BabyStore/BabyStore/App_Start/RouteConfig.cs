d1e3dcff7f4b561da52609d3ed7639001e18e533
Mohamed <mohamed.sellahi@gmail.com>
2017-11-14 22:20:05 +0100

    End of ch5

--- BabyStore/BabyStore/App_Start/RouteConfig.cs
+++ BabyStore/BabyStore/App_Start/RouteConfig.cs
@@ -5,19 +5,47 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace BabyStore
-{
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+namespace BabyStore {
+  public class RouteConfig {
+    public static void RegisterRoutes(RouteCollection routes) {
+      routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-        }
+      routes.MapRoute(
+        name:"ProductsCreate",
+        url:"Products/Create",
+        defaults: new {controller = "Products", action="Create"}
+        );
+      // route for /PRoducts/category/Page{X}
+
+      routes.MapRoute(
+        name:"ProductsByCategoryByPage",
+        url:"Products/{category}/Page{page}",
+        defaults: new {controller = "Products", action="Index"}
+        );
+
+      // route for : /Products/Page{X}
+      routes.MapRoute(
+        name:"ProductsByPage",
+        url:"Products/Page{page}",
+        defaults: new {controller = "Products",action="Index"}
+        );
+
+      routes.MapRoute(
+        name: "ProductsByCategory",
+        url: "Products/{category}",
+        defaults: new { controller = "Products", action="Index"}
+        );
+      routes.MapRoute(
+        name:"ProductsIndex",
+        url:"Products",
+        defaults: new {controller ="Products",action="Index"}
+        );
+
+      routes.MapRoute(
+          name: "Default",
+          url: "{controller}/{action}/{id}",
+          defaults: new { controller = "Home",action = "Index",id = UrlParameter.Optional }
+      );
     }
+  }
 }
